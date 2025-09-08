e1258cec3687939f2804ce5ea09260fa1dfdd89b
Svyatoslav Kozelchuk-Panulin <svyatoslavkozelchuk@gmail.com>
2025-09-09 01:20:29 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,49 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getActiveTodos, getCompletedTodos, getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [selectTodo, setSelectTodo] = useState<Todo | null>(null);
+  const [filter, setFilter] = useState('all');
+  const [textFilter, setTextFilter] = useState('');
+
+  useEffect(() => {
+    const loader =
+      filter === 'all'
+        ? getTodos
+        : filter === 'completed'
+          ? getCompletedTodos
+          : getActiveTodos;
+
+    loader()
+      .then(todos =>
+        todos.filter(todo =>
+          textFilter
+            ? todo.title.toLowerCase().includes(textFilter.toLowerCase())
+            : todo,
+        ),
+      )
+      .then(setTodos)
+      .catch(() => {
+        setTodos([]);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [filter, textFilter]);
+
+  const handleSelectTodo = (todo: Todo) => {
+    setSelectTodo(todo);
+  };
+
+  const handleCloseTodo = () => {
+    setSelectTodo(null);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +58,24 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                onSelect={value => setFilter(value)}
+                onQuery={query => setTextFilter(query)}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading ? (
+                <Loader />
+              ) : (
+                <TodoList todos={todos} onSelect={handleSelectTodo} />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      <TodoModal todo={selectTodo} onClose={handleCloseTodo} />
     </>
   );
 };
